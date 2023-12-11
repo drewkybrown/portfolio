ab7eb023743571deb0c378fcbdf5f2a52a3445f8
drewkybrown <146992520+drewkybrown@users.noreply.github.com>
2023-12-11 18:14:00 -0500

    change webpage title, replace vite logo with personal design, add home page introduction

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,19 @@
 function Home(props) {
-  return <h1>Home</h1>;
+  return (
+    <div className="intro">
+      <p className="greeting">Hi, my name is</p>
+      <h1 className="name">Drew, I code things for the web.</h1>
+      <p className="description">
+        I’m a software developer with a knack for backend technologies and a
+        comprehensive understanding of full-stack development. My coding
+        journey, ignited at General Assembly's bootcamp and enriched with my
+        past experience as a DJ, allows me to blend technical prowess with
+        creative thinking. Currently, I’m dedicated to developing accessible,
+        user-centric software solutions. Welcome to my world of innovative
+        coding!
+      </p>
+    </div>
+  );
 }
 
 export default Home;

3de70298a5ff0640370365c2413f2af1f34c3ffb
drewkybrown <146992520+drewkybrown@users.noreply.github.com>
2023-12-11 16:14:46 -0500

    Fetch and display data in about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,23 @@
-function About(props) {
-  return <h1>About</h1>;
+import { useState, useEffect } from "react";
+
+function About() {
+  const [about, setAbout] = useState([]);
+
+  useEffect(() => {
+    fetch("./about.json")
+      .then((res) => res.json())
+      .then((data) => setAbout(data));
+  }, []);
+
+  return about ? (
+    <div>
+      <h2>{about.name}</h2>
+      <img src={about.headshot} alt="Headshot" />
+      <p>{about.bio}</p>
+    </div>
+  ) : (
+    <h1>Loading...</h1>
+  );
 }
 
 export default About;

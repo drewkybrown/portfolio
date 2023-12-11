dfdc675c3f09f72142bb104f2d3b0630ba7d73c6
drewkybrown <146992520+drewkybrown@users.noreply.github.com>
2023-12-11 15:57:03 -0500

    create componenets and pages folders, files

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
-import { useState, useEffect } from "react";
-
 import "./App.css";
+
+// IMPORT COMPONENTS
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { Route, Routes } from "react-router-dom";
+
+// IMPORT PAGES
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
 
 function App() {
   return (
-    <>
-      <p>Hello Vite + React!</p>
-    </>
+    <div className="App">
+      <Header />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+      <Footer />
+    </div>
   );
 }
 

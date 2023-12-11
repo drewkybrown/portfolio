9ca454e8bde45b2601a0b57454d9dc9e57f44876
drewkybrown <146992520+drewkybrown@users.noreply.github.com>
2023-12-11 16:20:22 -0500

    add inline style to header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,27 @@
 import { Link } from "react-router-dom";
 
 function Header() {
-  <header>
-    <nav>
-      <Link to="/">Home</Link>
-      <Link to="/projects">Projects</Link>
-      <Link to="/about">About</Link>
-    </nav>
-  </header>;
+  // an example of an inline style for the nav tag for an initial layout
+
+  const navStyle = {
+    display: "flex",
+    justifyContent: "space-around",
+    borderBottom: ".2rem solid black",
+    padding: ".5rem",
+    width: "90%",
+    margin: "auto",
+  };
+
+  return (
+    <header>
+      <h1>My Portfolio Page</h1>
+      <nav style={navStyle}>
+        <Link to="/">HOME</Link>
+        <Link to="/projects">PROJECTS</Link>
+        <Link to="/about">ABOUT</Link>
+      </nav>
+    </header>
+  );
 }
 
 export default Header;

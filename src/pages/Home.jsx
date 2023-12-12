35bcbdf4f77db1c55dbfa3433b7c81b3472d1c7a
drewkybrown <146992520+drewkybrown@users.noreply.github.com>
2023-12-12 13:41:22 -0500

    Add custom styling to home component and move container in pages.css

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,23 +1,26 @@
 // src/pages/Home.jsx
 import "../styles/pages.css";
+import { useState, useEffect } from "react";
 
 // function to render the Home page
 function Home(props) {
   return (
-    <div className="p-5 mb-4 bg-light rounded-3 border">
-      <div className="container-fluid py-5">
-        <h1 className="display-5 fw-bold">
-          Hi! I'm Drew, I code things for the web.
-        </h1>
-        <p className="col-md-8 fs-4">
-          I’m a software developer with a knack for backend technologies and a
-          comprehensive understanding of full-stack development. My coding
-          journey, ignited at General Assembly's bootcamp and enriched with my
-          past experience as a DJ, allows me to blend technical prowess with
-          creative thinking. Currently, I’m dedicated to developing accessible,
-          user-centric software solutions. Welcome to my world of innovative
-          coding!
-        </p>
+    <div className="home-container">
+      <div className="p-5 mb-4 bg-light rounded-3 border">
+        <div className="container-fluid py-5">
+          <h1 className="home-heading display-5 fw-bold">
+            Hi! I'm Drew, I code things for the web.
+          </h1>
+          <p className="home-paragraph col-md-8 fs-4">
+            I’m a software developer with a knack for backend technologies and a
+            comprehensive understanding of full-stack development. My coding
+            journey, ignited at General Assembly's bootcamp and enriched with my
+            past experience as a DJ, allows me to blend technical prowess with
+            creative thinking. Currently, I’m dedicated to developing
+            accessible, user-centric software solutions. Welcome to my world of
+            innovative coding!
+          </p>
+        </div>
       </div>
     </div>
   );

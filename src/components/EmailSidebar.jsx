12e41bba0515ef10fa4c3983b35d6c57b56d8fd8
drewkybrown <146992520+drewkybrown@users.noreply.github.com>
2023-12-12 17:09:55 -0500

    overall styling to app

--- src/components/EmailSidebar.jsx
+++ src/components/EmailSidebar.jsx
@@ -1,14 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "../styles/components.css";
 
 function EmailSidebar() {
   return (
-    // Use Bootstrap classes along with other custom class
-    <div className="email-sidebar position-fixed top-0 end-0 bg-light border-start">
+    <div className="email-sidebar position-fixed top-0 end-0">
       <div className="email-container d-flex flex-column align-items-center justify-content-center h-100">
         <a
           href="mailto:andrew.brown.devs@gmail.com"
-          className="p-3 text-decoration-none text-dark"
+          className="email-text p-3 text-decoration-none email-text-custom"
         >
           andrew.brown.devs@gmail.com
         </a>
